0b84bc254f88c9afc0d5cd09f727c0aa31bf7c3f
mbjcodeverse <markhjolive@gmail.com>
2025-02-06 12:17:45 +0800

    modified AddTwoNumbers - added variables and display summation

--- AddTwoNumbers.java
+++ AddTwoNumbers.java
@@ -1,5 +1,7 @@
 public class AddTwoNumbers {
     public static void main(String[] args) {
-        
+        int num1 = 2;
+        int num2 = 5;
+        System.out.println("Sum = " + num1 + num2);
     }
 }
