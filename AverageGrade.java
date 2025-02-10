3b804b4954ee5b67f3bf44f5990ccaa3f45857a3
mbjcodeverse <markhjolive@gmail.com>
2025-02-10 10:30:02 +0800

    added code on AverageGrade to calculate average

--- AverageGrade.java
+++ AverageGrade.java
@@ -1,5 +1,8 @@
 public class AverageGrade {
     public static void main(String[] args) {
-        
+        int g1 = 89;
+        int g2 = 91;
+        int g3 = 87;
+        double average = (g1 + g2 + g3) / 3;
     }
 }
